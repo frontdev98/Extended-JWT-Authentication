17dcaf684df0e19f97a171c79862703113438311
Valentin Svirkiy <vsvirskiy98@gmail.com>
2025-06-22 17:29:28 +0300

    Create logger

---
+++ server/logger.js
@@ -0,0 +1,24 @@
+/*
+    Logger for info and error messages.
+    Store logs to file.
+    Store logs also to console if NODE_ENV='dev'
+*/
+const { createLogger, transports, format } = require('winston');
+
+const logger = createLogger({
+    defaultMeta: {'service': 'extended-jwt'},
+    format: format.json(),
+    transports: [
+        new transports.File({
+            filename: process.env.LOG_PATH
+        })
+    ]
+});
+
+if (process.env.NODE_ENV === 'dev') {
+    logger.add(new transports.Console({
+        format: format.simple()
+    }));
+}
+
+module.exports = logger;
