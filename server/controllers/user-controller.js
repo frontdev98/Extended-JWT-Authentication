0e9ffaaa54aa82196356fedf062c879af97e732d
Valentin Svirskiy <vsvirskiy98@gmail.com>
2025-06-23 16:31:11 +0300

    Finish registration method at UserController

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,4 +1,5 @@
 const { PrismaClient } = require('../generated/prisma/client')
+const userService = require('../services/user-service')
 const logger = require('../logger')
 
 const client = new PrismaClient();
@@ -6,9 +7,19 @@ const client = new PrismaClient();
 class UserController {
     async registration(req, res, next) {
         try {
-    
-        } catch (e) {
+            const {email, password} = req.body
+            const userData = await userService.registration(email, password)
 
+            res.cookie('refreshToken', userData.refreshToken, {
+                maxAge: 15 * 24 * 60 * 60 * 1000,   // max age is 15 days
+                httpOnly: true                      // don't allow getting this cookie with js
+                // secure: true                     // if you use https
+            })
+
+            return res.json(userData)
+
+        } catch (e) {
+            logger.error(e.message)
         }
     }
 
