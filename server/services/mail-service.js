d4835083d886ff91c748ce965a646b8298357a31
Valentin Svirskiy <vsvirskiy98@gmail.com>
2025-06-23 16:29:14 +0300

    Create mail service

---
+++ server/services/mail-service.js
@@ -0,0 +1,30 @@
+const nodemailer = require('nodemailer');
+
+class MailService {
+    constructor() {
+        this.transporter = nodemailer.createTransport({
+            service: 'yandex',
+            secure: false,
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASS
+            }
+        })
+    }
+    async sendActivationMail(to, link) {
+        await this.transporter.sendMail({
+            from: process.env.SMTP_USER,
+            to: to,
+            subject: 'Account activation on ' + process.env.API_URL,
+            text: '',
+            html:
+                `
+                    <div>
+                        <h1>Go to link for activation</h1>
+                        <a href="${link}">${link}</a>
+                `
+        })
+    }
+}
+
+module.exports = new MailService();
