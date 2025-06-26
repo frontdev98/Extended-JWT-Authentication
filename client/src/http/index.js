795385c7886dcde648cab6cf63282993aa5184a6
Valentin Svirskiy <vsvirskiy98@gmail.com>
2025-06-26 08:35:03 +0300

    Axios instance: 1. Create axios instance; 2. Add intercepter that includes header "Authorization" to client's request (takes it from localStorage)

---
+++ client/src/http/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+export const API_URL = `http://localhost:5000/api`
+
+const $api = axios.create({
+    withCredentials: true,  // cookie assigned automatically
+    baseURL: API_URL        // this string will be added to URL during each request
+})
+
+// intercepter for each request from the client:
+// - add header "Authorization" with payload (fetch that from localStorage)
+$api.interceptors.request.use((requestConfig) => {
+    requestConfig.headers.Authorization = `Bearer ${localStorage.getItem('accessToken')}`;
+    return token;
+})
+
+export default $api;
